249e19d5c920ca84c08c5b919b4f0b36ef109e1e
pranav <psoniradhe177@gmail.com>
2026-03-11 14:40:47 +0530

    Added terraform/backend.tf file

---
+++ terraform/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    # Required parameters
+    bucket = "app-s3-backend"
+    key    = "terraform/terraform.tfstate"
+    region = "ap-south-1"
+
+    # Optional parameters (recommended for team environments)
+    encrypt        = true
+    use_lockfile = true # Alternative to dynamodb_table, available in newer Terraform versions
+  }
+}
